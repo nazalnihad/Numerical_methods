0157aa1fcf33eae1c47b96cdf2faac6152fc761b
Nazal-nihad <nazalnihad17@gmail.com>
2024-01-24 15:30:46 +0530

    iteration count

--- bisectionMethod.py
+++ bisectionMethod.py
@@ -1,6 +1,5 @@
-def bisection(f, a, b, e):
+def bisection(f, a, b, e, iteration=1):
     c = (a+b)/2
-
     if (b-c <= e):
         return c
     elif (f(b)*f(c) <= 0):
@@ -8,21 +7,29 @@ def bisection(f, a, b, e):
     else:
         b = c
 
-    return bisection(f, a, b, e)
+    print(f"====={iteration}=====")
+    print("a =", a)
+    print("b =", b)
+    print("c =", c)
+    print("==========")
+
+    return bisection(f, a, b, e, iteration+1)
 
 
 def func(x):
-    return x**3-4*x-9
+    return x**6-x-1
 
 
 def initial_vals():
     c = 0
     a = func(c)
     b = func(c+1)
+    i = 0
     while (True):
         if (a < 0 and b > 0):
             return a, b
         else:
+            i += 1
             c += 1
             a = b
             b = func(c)
